3d8fe2645d1a3edaf94145cc01dc983f8e252e44
wildanaditya <35674207+wildanaditya@users.noreply.github.com>
2018-04-01 21:46:33 +0700

    terakhir

---
+++ app/src/main/java/com/wildanaditya_1202150038_modul6/homeScreen/FragmentMyPost.java
@@ -0,0 +1,80 @@
+package com.wildanaditya_1202150038_modul6.homeScreen;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import com.wildanaditya_1202150038_modul6.MainActivity;
+import com.wildanaditya_1202150038_modul6.R;
+import com.wildanaditya_1202150038_modul6.adapter.PostAdapter;
+import com.wildanaditya_1202150038_modul6.model.Post;
+
+import java.util.ArrayList;
+
+
+public class FragmentMyPost extends Fragment {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+
+    private ArrayList<Post> listPosts;
+
+    Query databaseFood;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_my_post, container, false);
+
+        recyclerView = view.findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+
+        databaseFood = FirebaseDatabase.getInstance().getReference(MainActivity.table1).orderByChild("userID").equalTo(mAuth.getUid());
+
+        listPosts = new ArrayList<>() ;
+
+        return view;
+    }
+
+    public void onStart() {
+        super.onStart();
+        databaseFood.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                listPosts.clear();
+
+                for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+
+                    Post post = postSnapshot.getValue(Post.class);
+
+                    listPosts.add(post);
+                }
+
+                recyclerView.setLayoutManager(new GridLayoutManager(getContext(),2));
+
+                PostAdapter postList = new PostAdapter(getContext(), listPosts);
+
+                recyclerView.setAdapter(postList);
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/wildanaditya_1202150038_modul6/model/User.java
@@ -0,0 +1,29 @@
+package com.wildanaditya_1202150038_modul6.model;
+
+public class User {
+
+    String userId;
+    String username;
+    String email;
+
+    public User() {
+    }
+
+    public User(String userId, String username, String email) {
+        this.userId = userId;
+        this.username = username;
+        this.email = email;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
